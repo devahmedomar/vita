3819f9c09a1b373a9ce60a0784da046907ae6565
mahmoud khaled <m01115626084@gmail.com>
2024-06-10 10:59:22 +0300

    request api shop

--- src/app/modules/shared/components/productcard/productcard.component.ts
+++ src/app/modules/shared/components/productcard/productcard.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { iProduct } from '../../../../models/iproduct';
-
+import { ProductServiseService } from 'src/app/product-servise.service';
 @Component({
   selector: 'app-productcard',
   templateUrl: './productcard.component.html',
   styleUrls: ['./productcard.component.css']
 })
 export class ProductcardComponent implements OnInit {
+  DataOfProduct:any=[];
+
   @Input() productCardData:iProduct={
     name:'',
     description:'',
@@ -27,13 +29,31 @@ export class ProductcardComponent implements OnInit {
     weight:0
 
   }
+  @Input() CardData=
+  {
+    id:0,
+    haveSale: false,
+    imgURL: "",
+    productName: "",
+    productDescription: "",
+    productPrice: 0,
+    productRate: 0,
+    sale: 0
+  }
   @Input() origin: string="";
-  constructor() { }
+  constructor(public _ProductServiseService:ProductServiseService ) {
+   }
 
   ngOnInit() {
+    // this._ProductServiseService.GetDataOfProduct(this.DataOfProduct).subscribe((data) =>
+    //   {
+    //     this.DataOfProduct=data.results;
+    //   });
+    console.log(this.CardData)
+    console.log(localStorage.getItem('eToken'))
 
   }
-
+//////!
   getRouterLink(): string {
     if (this.origin === 'home') {
       return `shop/${this.productCardData.productId}`;
--- src/app/modules/shop/components/shop/shop.component.ts
+++ src/app/modules/shop/components/shop/shop.component.ts
@@ -1,6 +1,9 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Ibreadcrumb } from 'src/app/models/ibreadcrumb';
 import { Iproductcard } from 'src/app/models/iproductcard';
+import { ProductServiseService } from 'src/app/product-servise.service';
+
 
 @Component({
   selector: 'app-shop',
@@ -8,79 +11,96 @@ import { Iproductcard } from 'src/app/models/iproductcard';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
+  constructor(public _ProductServiseService:ProductServiseService){}
   shopBreadCrumbData: Ibreadcrumb = {
     title: "shop",
     prev: "home"
   }
-  products: Iproductcard[] = []
+  products: any[] = []
 
 
 
 
   ngOnInit(): void {
-    // this.products = [
-    //   {
-    //     id:1,
-    //     haveSale: true,
-    //     imgURL: "assets/images/product.png",
-    //     productName: "Airbrush Matte",
-    //     productDescription: "Skin-perfecting bronzed filter for the face.",
-    //     productPrice: 50.00,
-    //     productRate: 4,
-    //     sale: 15
-    //   },
-    //   {
-    //     id:2,
-    //     haveSale: true,
-    //     imgURL: "assets/images/product.png",
-    //     productName: "Airbrush Matte",
-    //     productDescription: "Skin-perfecting bronzed filter for the face.",
-    //     productPrice: 60.00,
-    //     productRate: 3,
-    //     sale: 14
-    //   },
-    //   {
-    //     id:3,
-    //     haveSale: false,
-    //     imgURL: "assets/images/product.png",
-    //     productName: "Airbrush Matte",
-    //     productDescription: "Skin-perfecting bronzed filter for the face.",
-    //     productPrice: 100.00,
-    //     productRate: 2,
-    //   },
-    //   {
-    //     id:4,
-    //     haveSale: true,
-    //     imgURL: "assets/images/product.png",
-    //     productName: "Airbrush Matte",
-    //     productDescription: "Skin-perfecting bronzed filter for the face.",
-    //     productPrice: 50.00,
-    //     productRate: 4,
-    //     sale: 15
-    //   },
-    //   {
-    //     id:5,
-    //     haveSale: false,
-    //     imgURL: "assets/images/product.png",
-    //     productName: "Airbrush Matte",
-    //     productDescription: "Skin-perfecting bronzed filter for the face.",
-    //     productPrice: 60.00,
-    //     productRate: 3,
-    //   },
-    //   {
-    //     id:6,
-    //     haveSale: true,
-    //     imgURL: "assets/images/product.png",
-    //     productName: "Airbrush Matte",
-    //     productDescription: "Skin-perfecting bronzed filter for the face.",
-    //     productPrice: 100.00,
-    //     productRate: 2,
-    //     sale: 10
-    //   },
+  //   this.products = [
+  //     {
+  //       id:1,
+  //       haveSale: true,
+  //       imgURL: "assets/images/product.png",
+  //       productName: "Airbrush Matte",
+  //       productDescription: "Skin-perfecting bronzed filter for the face.",
+  //       productPrice: 50.00,
+  //       productRate: 4,
+  //       sale: 15
+  //     },
+  //     {
+  //       id:2,
+  //       haveSale: true,
+  //       imgURL: "assets/images/product.png",
+  //       productName: "Airbrush Matte",
+  //       productDescription: "Skin-perfecting bronzed filter for the face.",
+  //       productPrice: 60.00,
+  //       productRate: 3,
+  //       sale: 14
+  //     },
+  //     {
+  //       id:3,
+  //       haveSale: false,
+  //       imgURL: "assets/images/product.png",
+  //       productName: "Airbrush Matte",
+  //       productDescription: "Skin-perfecting bronzed filter for the face.",
+  //       productPrice: 100.00,
+  //       productRate: 2,
+  //     },
+  //     {
+  //       id:4,
+  //       haveSale: true,
+  //       imgURL: "assets/images/product.png",
+  //       productName: "Airbrush Matte",
+  //       productDescription: "Skin-perfecting bronzed filter for the face.",
+  //       productPrice: 50.00,
+  //       productRate: 4,
+  //       sale: 15
+  //     },
+  //     {
+  //       id:5,
+  //       haveSale: false,
+  //       imgURL: "assets/images/product.png",
+  //       productName: "Airbrush Matte",
+  //       productDescription: "Skin-perfecting bronzed filter for the face.",
+  //       productPrice: 60.00,
+  //       productRate: 3,
+  //     },
+  //     {
+  //       id:6,
+  //       haveSale: true,
+  //       imgURL: "assets/images/product.png",
+  //       productName: "Airbrush Matte",
+  //       productDescription: "Skin-perfecting bronzed filter for the face.",
+  //       productPrice: 100.00,
+  //       productRate: 2,
+  //       sale: 10
+  //     },
 
-    // ]
+  //   ]
+        let api_key = localStorage.getItem('eToken');
+        const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${api_key}`});
+        const requestOptions = { headers: headers };
 
-  }
+    this._ProductServiseService.GetDataOfProduct(requestOptions).subscribe((data) =>
+      {
+        console.log(data.data)
+      
+        this.products=data.data.products;
+      });
+    // (Error));
+    //   // console.log(localStorage.getItem('eToken'))
+    //   console.log(this.products)
 
+  
+  
+    }
 
 }
