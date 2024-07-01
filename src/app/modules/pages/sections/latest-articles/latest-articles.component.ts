c376b213202bbf8252d7db4d8251e8a9fab34b28
Ahmed Omar Abdel Hakim Morsi <159171834+devahmedomar@users.noreply.github.com>
2024-07-01 13:51:18 +0300

    changes 1-7

--- src/app/modules/pages/sections/latest-articles/latest-articles.component.ts
+++ src/app/modules/pages/sections/latest-articles/latest-articles.component.ts
@@ -2,6 +2,8 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Blog, posts } from 'src/app/models/blog';
 import { BlogService } from 'src/app/services/blog/blog.service';
+import { Subscription } from 'rxjs';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-latest-articles',
@@ -10,8 +12,8 @@ import { BlogService } from 'src/app/services/blog/blog.service';
 })
 export class LatestArticlesComponent implements OnInit {
   blogPosts: posts[] = [];
-
-  constructor(private _BlogService:BlogService, private router: Router){}
+  private langChangeSubscription: Subscription=new Subscription();
+  constructor(private _BlogService:BlogService, private router: Router,private translate: TranslateService){}
   getBlogs(){
     this._BlogService.getBlogs().subscribe({
       next:(res) => {
@@ -26,6 +28,16 @@ export class LatestArticlesComponent implements OnInit {
   }
   ngOnInit(): void {
     this.getBlogs();
+    // Subscribe to language change events
+    this.langChangeSubscription = this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.getBlogs();
+    });
+  }
+  ngOnDestroy(): void {
+    // Unsubscribe from language change events when the component is destroyed
+    if (this.langChangeSubscription) {
+      this.langChangeSubscription.unsubscribe();
+    }
   }
   navigateToBlog(blogPostId: number) {
     this.router.navigate(['/blog', blogPostId]);
