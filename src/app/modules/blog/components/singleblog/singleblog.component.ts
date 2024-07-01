cc719479f3bcfbf8527aa39319246654510ed9af
Israa Gamal <isrraa.gamal@gmail.com>
2024-07-01 11:31:56 +0300

    Like/Dislike/Unlike/Share - Categories

--- src/app/modules/blog/components/singleblog/singleblog.component.ts
+++ src/app/modules/blog/components/singleblog/singleblog.component.ts
@@ -38,19 +38,13 @@ export class SingleblogComponent implements OnInit {
           next: (response) => {
             this.blog = response.data.post;
             this.safeBlogContent = this._sanitizer.bypassSecurityTrustHtml(this.blog.content);
-
             this.blog.likes = this.blog.likes || 0;
             this.blog.dislikes = this.blog.dislikes || 0;
-
-            const storedLikes = localStorage.getItem(this.localStorageKey);
-            if (storedLikes) {
-              const likesData = JSON.parse(storedLikes);
-              if (likesData[this.blog.blogPostId]) {
-                this.blog.likes = likesData[this.blog.blogPostId].likes || this.blog.likes;
-                this.blog.dislikes = likesData[this.blog.blogPostId].dislikes || this.blog.dislikes;
-                this.alreadyLiked = likesData[this.blog.blogPostId].liked || false;
-                this.alreadyDisliked = likesData[this.blog.blogPostId].disliked || false;
-              }
+            const storedState = localStorage.getItem(`blog-${blogId}-like-dislike`);
+            if (storedState) {
+              const { liked, disliked } = JSON.parse(storedState);
+              this.alreadyLiked = liked;
+              this.alreadyDisliked = disliked;
             }
           },
           error: (error) => {
@@ -69,72 +63,75 @@ export class SingleblogComponent implements OnInit {
   likeBlog(): void {
     if (this.isLoggedIn && this.blog && this.userToken) {
       if (!this.alreadyLiked) {
-        if (this.alreadyDisliked) {
-          this.blog.dislikes--;
-          this.alreadyDisliked = false;
-        }
-
-        this.blog.likes++;
-        this.alreadyLiked = true;
-        this.updateLocalStorage('like');
+        this._BlogService.likePost(this.blog.blogPostId, this.userToken).subscribe({
+          next: () => {
+            if (this.alreadyDisliked) {
+              this.blog.dislikes--;
+              this.alreadyDisliked = false;
+            }
+            this.blog.likes++;
+            this.alreadyLiked = true;
+            this.updateLocalStorage();
+          },
+       
+        });
       } else {
-        this.blog.likes--;
-        this.alreadyLiked = false;
-        this.updateLocalStorage('like');
+        this._BlogService.unlikePost(this.blog.blogPostId, this.userToken).subscribe({
+          next: () => {
+            this.blog.likes--;
+            this.alreadyLiked = false;
+            this.updateLocalStorage();
+          },
+      
+        });
       }
-    } else {
-      console.error('User not logged in or Blog ID is not available!');
+    } else if (!this.isLoggedIn) {
       alert('You need to be logged in to like this post.');
     }
   }
-  
+
   dislikeBlog(): void {
     if (this.isLoggedIn && this.blog && this.userToken) {
       if (!this.alreadyDisliked) {
-        if (this.alreadyLiked) {
-          this.blog.likes--;
-          this.alreadyLiked = false;
-        }
-  
-        this.blog.dislikes++;
-        this.alreadyDisliked = true;
-        this.updateLocalStorage('dislike');
+        this._BlogService.dislikePost(this.blog.blogPostId, this.userToken).subscribe({
+          next: () => {
+            if (this.alreadyLiked) {
+              this.blog.likes--;
+              this.alreadyLiked = false;
+            }
+            this.blog.dislikes++;
+            this.alreadyDisliked = true;
+            this.updateLocalStorage();
+          },
+      
+        });
       } else {
-        this.blog.dislikes--; 
-        if (this.blog.dislikes < 0) {
-          this.blog.dislikes = 0;
-        }
-        this.alreadyDisliked = false;
-        this.updateLocalStorage('dislike');
+        this._BlogService.unlikePost(this.blog.blogPostId, this.userToken).subscribe({
+          next: () => {
+            this.blog.dislikes--;
+            this.alreadyDisliked = false;
+            this.updateLocalStorage();
+          },
+
+        });
       }
-    } else {
-      console.error('User not logged in or Blog ID is not available!');
+    } else if (!this.isLoggedIn) {
       alert('You need to be logged in to dislike this post.');
     }
   }
-  
-  
-  private updateLocalStorage(action: string): void {
-    const storedLikes = localStorage.getItem(this.localStorageKey);
-    let likesData = storedLikes ? JSON.parse(storedLikes) : {};
 
-    if (!likesData[this.blog.blogPostId]) {
-      likesData[this.blog.blogPostId] = { likes: 0, dislikes: 0 };
-    }
-    if (action === 'like') {
-      likesData[this.blog.blogPostId].likes = this.blog.likes;
-      likesData[this.blog.blogPostId].liked = this.alreadyLiked;
+  private updateLocalStorage(): void {
+    const blogId = this.blog.blogPostId;
+    const state = {
+      liked: this.alreadyLiked,
+      disliked: this.alreadyDisliked,
+    };
+    localStorage.setItem(`blog-${blogId}-like-dislike`, JSON.stringify(state));
+  }
 
-      likesData[this.blog.blogPostId].dislikes = this.blog.dislikes;
-      likesData[this.blog.blogPostId].disliked = this.alreadyDisliked;
-    } else if (action === 'dislike') {
-      likesData[this.blog.blogPostId].dislikes = this.blog.dislikes;
-      likesData[this.blog.blogPostId].disliked = this.alreadyDisliked;
-
-      likesData[this.blog.blogPostId].likes = this.blog.likes;
-      likesData[this.blog.blogPostId].liked = this.alreadyLiked;
-    }
-
-    localStorage.setItem(this.localStorageKey, JSON.stringify(likesData));
+  getFacebookShareUrl(blogId:number): string {
+    const blogUrl = `https://Vita-domain.com/blog/${blogId}`;
+    const shareUrl = `https://www.facebook.com/sharer.php?u=${encodeURIComponent(blogUrl)}`;
+    return shareUrl;
   }
 }
